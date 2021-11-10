b9ea63e8523d7ce2ae0ae74c30b8c96597a80a00
changsik <lowmans00@gmail.com>
2021-11-10 17:54:38 +0900

    feat: add first md for test
    https://nuxtjs.org/tutorials/creating-blog-with-nuxt-content/

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,28 +3,22 @@ export default {
   head: {
     title: 'nuxt-blog',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'ant-design-vue/dist/antd.css'
-  ],
+  css: ['ant-design-vue/dist/antd.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/antd-ui'
-  ],
+  plugins: ['@/plugins/antd-ui'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -52,6 +46,5 @@ export default {
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,40 @@
+<template>
+  <article>
+    <!-- <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p> -->
+    <pre> {{ article }} </pre>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+    return { article }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
+  },
+}
+</script>
+<style>
+.nuxt-content h2 {
+  font-weight: bold;
+  font-size: 28px;
+  color: red;
+}
+.nuxt-content h3 {
+  font-weight: bold;
+  font-size: 22px;
+  color: red;
+}
+.nuxt-content p {
+  margin-bottom: 20px;
+}
+</style>
